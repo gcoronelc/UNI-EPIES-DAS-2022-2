5b9ff3b5f99bae1841cbacd16e0c767c08a8ba43
Gustavo Corone <gcoronelc@gmail.com>
2022-12-04 18:55:12 -0500

    Seminario

---
+++ ProyectoDemo/VentaPasajes/src/pe/edu/uni/ventapasajes/controller/LogonController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.uni.ventapasajes.controller;
+
+import pe.edu.uni.ventapasajes.model.UsuarioModel;
+import pe.edu.uni.ventapasajes.service.LogueoService;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public class LogonController {
+   
+   public void validar(String usuario, String clave) {
+      LogueoService service = new LogueoService();
+      UsuarioModel usuarioModel = service.validar(usuario, clave);
+      if(usuarioModel==null){
+         throw  new RuntimeException("Datos incorrectos.");
+      }
+   }
+   
+}
